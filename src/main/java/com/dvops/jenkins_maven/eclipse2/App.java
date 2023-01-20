a7a3848e37dae9c83ad5b732d822ced19b669790
zhaohonglau <lau_zhao_hong@myrp.edu.sg>
2023-01-20 13:08:11 +0800

    Initial Commit

---
+++ src/main/java/com/dvops/jenkins_maven/eclipse2/App.java
@@ -0,0 +1,13 @@
+package com.dvops.jenkins_maven.eclipse2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
